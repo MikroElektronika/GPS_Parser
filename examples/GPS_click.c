e07aa6e9a5219bf7dd721d72f2aa683975a7a34e
Richard Lowe <ocdrichard@gmail.com>
2016-03-09 10:18:19 +0100

    Update example.

--- examples/GPS_click.c
+++ examples/GPS_click.c
@@ -1,21 +1,72 @@
+/*******************************************************************************
+* Title                 :   GPS Parser Demo
+* Filename              :   GPS_click.c
+* Author                :   RBL
+* Origin Date           :   09/03/2015
+* Notes                 :   None
+*******************************************************************************/
+/*************** MODULE REVISION LOG ******************************************
+*
+*    Date    Software Version    Initials   Description 
+*  09/03/15         .1           RBL        Module Created.
+*
+*******************************************************************************/
+/** 
+ * @file GPS_click.c
+ * @brief Example to the GPS Parser
+ */
+/******************************************************************************
+* Includes
+*******************************************************************************/
 #include "scheduler.h"
 #include "gps_parser.h"
 
+/******************************************************************************
+* Module Preprocessor Constants
+*******************************************************************************/
+
+
+/******************************************************************************
+* Module Preprocessor Macros
+*******************************************************************************/
 #define MSG( TXT ) UART1_Write_Text( TXT )
 
+/******************************************************************************
+* Module Typedefs
+*******************************************************************************/
+
+/******************************************************************************
+* Module Variable Definitions
+*******************************************************************************/
 sbit STATUS_LED at GPIOB_ODR.B0;
-sbit RX_LED at GPIOB_ODR.B1;
 sbit POWER at GPIOA_ODR.B0;
 sbit RESET at GPIOC_ODR.B2;
 sbit WAKEUP at GPIOA_ODR.B4;
 
-void heartbeat()
+/******************************************************************************
+* Function Prototypes
+*******************************************************************************/
+// Used for visual confirmation of system running
+static void heartbeat( void );
+// Checks for valid gps messages
+static void check_gps( void );
+// Timer for scheduler
+static void init_timer2( void );
+// Initialize Nano GPS
+static void gps_initialize( void );
+// Initialize system
+static void system_init( void );
+
+/******************************************************************************
+* Function Definitions
+*******************************************************************************/
+static void heartbeat()
 {
     STATUS_LED = ~STATUS_LED;
 }
 
 
-void check_gps()
+static void check_gps()
 {
     char text[80];
     location_t *me;
@@ -50,7 +101,7 @@ void check_gps()
 }
 
 //Timer2 Prescaler :575; Preload = 62499; Actual Interrupt Time = 500 ms
-void init_timer2()
+static void init_timer2()
 {
     RCC_APB1ENR.TIM2EN = 1;
     TIM2_CR1.CEN = 0;
@@ -60,7 +111,7 @@ void init_timer2()
     TIM2_CR1.CEN = 1;
 }
 
-void gps_initialize()
+static void gps_initialize()
 {
     RESET = 1;
     Delay_ms( 300 );
@@ -74,7 +125,7 @@ void gps_initialize()
     Delay_ms( 100 );
 }
 
-void system_init()
+static void system_init()
 {
     GPIO_Digital_Output( &GPIOA_BASE, _GPIO_PINMASK_0 );
     GPIO_Digital_Output( &GPIOB_BASE, _GPIO_PINMASK_0 | _GPIO_PINMASK_1 );
@@ -84,28 +135,19 @@ void system_init()
 
     UART1_Init_Advanced( 115200, _UART_8_BIT_DATA, _UART_NOPARITY,
                          _UART_ONE_STOPBIT, &_GPIO_MODULE_USART1_PA9_10 );
-    Delay_ms( 100 );
     RXNEIE_USART1_CR1_bit = 1;
 
     UART2_Init_Advanced(  9600, _UART_8_BIT_DATA, _UART_NOPARITY,
                           _UART_ONE_STOPBIT, &_GPIO_MODULE_USART2_PD56 );
 
-    Delay_ms( 100 );
     RXNEIE_USART2_CR1_bit = 1;
-
-    UART3_Init_Advanced( 9600, _UART_8_BIT_DATA, _UART_NOPARITY,
-                         _UART_ONE_STOPBIT, &_GPIO_MODULE_USART3_PD89 );
-
     Delay_ms( 100 );
-    //RXNEIE_USART3_CR1_bit = 1;
-
+        
     init_timer2();
     task_scheduler_init( 500 );
-    //gps_initialize();
+    gps_initialize();
 
-    NVIC_IntEnable( IVT_INT_USART1 );
     NVIC_IntEnable( IVT_INT_USART2 );
-    //NVIC_IntEnable( IVT_INT_USART3 );
     NVIC_IntEnable(IVT_INT_TIM2);
 
     EnableInterrupts();
@@ -126,33 +168,11 @@ void main()
     }
 }
 
-void UART1_RX_ISR() iv IVT_INT_USART1 ics ICS_AUTO
-{
-    if( RXNE_USART1_SR_bit )
-    {
-        UART1_Write( USART1_DR );
-        RX_LED = ~RX_LED;
-    }
-}
-
-
 void UART2_RX_ISR() iv IVT_INT_USART2 ics ICS_AUTO
 {
     if( RXNE_USART2_SR_bit )
     {
         char tmp = USART2_DR;
-        gps_put( tmp );
-        //UART1_Write( tmp );
-        RX_LED = ~RX_LED;
-    }
-}
-
-
-void UART3_RX_ISR() iv IVT_INT_USART3 ics ICS_AUTO
-{
-    if( RXNE_USART3_SR_bit )
-    {
-        char tmp = USART3_DR;
         gps_put( tmp );
         RX_LED = ~RX_LED;
     }
@@ -163,3 +183,6 @@ void Timer2_interrupt() iv IVT_INT_TIM2
     TIM2_SR.UIF = 0;
     task_scheduler_clock();
 }
+
+
+/*************** END OF FUNCTIONS ***************************************************************************/
